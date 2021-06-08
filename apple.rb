42ffb0a149e87e5dbfc6abaced20d491bd736232
EC2 Default User <ec2-user@ip-172-31-50-218.ec2.internal>
2021-06-08 00:32:03 +0000

    ENSYU6 1-6 Finish

---
+++ apple.rb
@@ -0,0 +1,17 @@
+apple = "Nagano"
+if apple =="Aomori"
+  puts "1.このリンゴは青森県産です"
+elsif apple == "Nagano"
+  puts "2.このリンゴは青森県産ではなく、長野県産です"
+else
+  puts "3.このリンゴは青森県産でも長野県でもありません"
+end
+  
+orange = "YAMANASHI"
+if orange =="Aomori"
+  puts "1.このオレンジは青森県産です"
+elsif orange== "Nagano"
+  puts "2.このオレンジは青森県産ではなく、長野県産です"
+else
+  puts "3.このオレンジは青森県産でも長野県でもありません"
+end
---
+++ hand.rb
@@ -0,0 +1,12 @@
+hand ="グー"
+if hand =="グー"
+  puts "出したてはグーです"
+end
+
+if hand !="チョキ"
+  puts "出したてはチョキではありません"
+end
+
+if (hand=="グー")||(hand=="パー")
+  puts "出したてはグーあたはパーです"
+end
